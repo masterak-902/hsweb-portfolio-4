6d5b687ca1e6f2f588a70e19a8d8c52ab77c7cab
masterak-902 <hsn-software@masterak.jp>
2024-08-28 19:26:30 +0900

    update reset css

--- src/components/ContactMessageForm.tsx
+++ src/components/ContactMessageForm.tsx
@@ -1,7 +1,13 @@
 export default function ContactMessageForm() {
   return (
     <>
-        <p>React Component</p>            
+        <h2>お問い合わせフォーム</h2>
+        <form>
+          <label htmlFor="email">メールアドレス</label>
+          <input type="email" id="email" name="email" />
+          <label htmlFor="message">お問い合わせ内容</label>
+          <textarea id="message" name="message" />
+        </form>        
     </>
   );
 }
