27718fbe95f8b2a61802b147fd7ac89ec3a01875
masterak-902 <hsn-software@masterak.jp>
2024-09-08 21:50:22 +0900

    add : CF adapter SSR , Zenn新着記事 のレイアウトを変更

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,12 @@ import { defineConfig } from 'astro/config';
 
 import preact from "@astrojs/preact";
 
+import cloudflare from '@astrojs/cloudflare';
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [preact()],
+
   i18n: {
     defaultLocale: 'ja',
     locales: ['ja', 'en'],
@@ -14,5 +17,8 @@ export default defineConfig({
     fallback: {
       en: 'ja'
     }
-  }
+  },
+
+  output: 'hybrid',
+  adapter: cloudflare()
 });
